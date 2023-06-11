3e8c8413418c04302483dba0e964d4be558ac3a3
mPenTests <marko.prifti4@gmail.com>
2023-06-11 16:07:20 +0200

    Added User Profile model

--- chat/models.py
+++ chat/models.py
@@ -6,9 +6,15 @@ class User(AbstractUser):
     pass
 
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=500)
+    friends = models.ManyToManyField('self')
+    created_at = models.DateTimeField(auto_now_add=True)
+
 class Message(models.Model):
-    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
-    to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
+    from_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='sender')
+    to_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='receiver')
     message = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
     
@@ -16,5 +22,5 @@ class Message(models.Model):
 class Group(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
-    users = models.ManyToManyField(User)
+    users = models.ManyToManyField(UserProfile)
     messages = models.ManyToManyField(Message)
