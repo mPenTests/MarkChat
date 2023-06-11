324ea7fec6c4523918f94ca0533a5720f5899548
mPenTests <marko.prifti4@gmail.com>
2023-06-11 16:02:30 +0200

    Configured Models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    pass
+
+
+class Message(models.Model):
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
+    message = models.CharField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    users = models.ManyToManyField(User)
+    messages = models.ManyToManyField(Message)
